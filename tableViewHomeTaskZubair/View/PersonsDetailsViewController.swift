0b98dd130863ca58b5a197133e4547ce24d60919
midniqhtman <c1esi@mail.ru>
2022-10-20 01:35:55 +0300

    finished DetailsVC

--- tableViewHomeTaskZubair/View/PersonsDetailsViewController.swift
+++ tableViewHomeTaskZubair/View/PersonsDetailsViewController.swift
@@ -12,10 +12,14 @@ class PersonsDetailsViewController: UIViewController {
     var details: Persons!
     
     @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var numberLabel: UILabel!
+    @IBOutlet var mailLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         nameLabel.text = details.name
+        numberLabel.text = String(details.number)
+        mailLabel.text = details.email
     }
     
 
